fc2837477cd4467faa42484f9aeaee5180abf1ea
Ujjwal Ojha <bring2uo@gmail.com>
2026-05-02 18:49:06 +1000

    fix lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,5 +3,8 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  "*.ts": ["oxfmt --write", "sh -c 'PATH=\"$PATH:./node_modules/.bin\" NODE_OPTIONS=\"--import tsx/esm\" oxlint --disable-nested-config --fix \"$@\"' sh"]
+  "*.ts": [
+    "oxfmt --write",
+    'sh -c \'PATH="$PATH:./node_modules/.bin" NODE_OPTIONS="--import tsx/esm" oxlint --disable-nested-config --fix "$@"\' sh'
+  ]
 };
